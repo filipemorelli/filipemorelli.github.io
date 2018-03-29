c4760a166555b9cc8b697dd7c92da937afeb672c
Filipe Morelli <morellitecinfo@gmail.com>
2018-03-29 11:48:00 -0300

    Gulp Completo

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp');
+var purify = require('gulp-purifycss');
+var concat = require('gulp-concat');
+var minifyCSS = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var changed = require('gulp-changed');
+var imagemin = require('gulp-imagemin');
+
+gulp.task('css', function () {
+    return gulp.src('./app/assets/css/*.css')
+        .pipe(purify(['./app/assets/**/*.js', './app/**/*.html']))
+        .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('fonts', function () {
+    return gulp.src('./app/assets/fonts/*')
+        .pipe(gulp.dest('public/fonts'));
+});
+
+gulp.task('styles', ['fonts', 'css'], function () {
+    return gulp.src('dist/css/*.css')
+        .pipe(concat('site.css'))
+        .pipe(minifyCSS())
+        .pipe(autoprefixer('last 2 versions'))
+        .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('js', function () {
+    return gulp.src('./app/assets/js/*.js')
+        .pipe(concat('scripts.js'))
+        .pipe(gulp.dest('dist/js'))
+        .pipe(rename('scripts.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('public/js'));
+});
+
+gulp.task('img', function () {
+    gulp.src('./app/assets/img/**/*.+(png|jpg|gif)')
+        .pipe(changed('public/assets/img'))
+        .pipe(imagemin())
+        .pipe(gulp.dest('public/assets/img'));
+});
